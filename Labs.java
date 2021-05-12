14d25d9ad26abd5adcc69b691b36aaa47947b41d
Tyler Throop <tyler.throop@gmail.com>
2021-05-12 12:42:46 -0700

    Pushing up the code i used for my labs it will work in eclipse but might not run well on other IDE

---
+++ Labs.java
@@ -0,0 +1,63 @@
+package Revature_Code;
+
+public package com.revature.primitives;
+
+public class Labs {
+	
+public static void main(String[] args) {
+	System.out.println("Hello");
+	System.out.println("Tyler Throop");
+	String firstName = "Tyler";
+	String lastName = "Throop";
+	System.out.println(firstName + lastName);
+	boolean on = true;
+	System.out.println(on);
+	short s = 16 ;
+	System.out.println(s);
+	int i = -32;
+    System.out.println(i);
+    float f = 3839.34839f;
+    System.out.println(f);
+    long l = 4294967296l;
+    System.out.println(l);
+    char c = 'a';
+    System.out.println(c);
+    
+    int x = 5;
+    int y = 3;
+    int z;
+    z = x+y;
+    System.out.println(z);
+    z = x-y;
+    System.out.println(z);
+    z = x*y;
+    System.out.println(z);
+    z = x/y;
+    System.out.println(z);
+    z = x%y;
+    System.out.println(z);
+    int ex = 200;
+    short se = (short)ex;
+    System.out.println(se);
+    
+    boolean bool = true;
+    boolean secondCondition = true;
+    if(bool) {
+    	System.out.println("in the if");
+    	if(secondCondition) {
+    		System.out.println("in the second conditional");
+    	}
+    }else {
+    	System.out.println("in the else");
+    }
+    System.out.println("outside the if");
+    int precipitation = 0;
+    int waterOutput = 0;
+    if(precipitation < 20) {
+    	waterOutput = 30;
+    }
+}
+}
+class Labs {
+    
+}
